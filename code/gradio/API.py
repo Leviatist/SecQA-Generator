b2217206571ad48944ddc2f7b03f0780450a4b2f
Lev <leviatist@gmail.com>
2025-08-22 12:59:46 +0800

    update 08222025

---
+++ code/gradio/API.py
@@ -0,0 +1,2 @@
+DEEPSEEK_API_KEY = "sk-9147b6e1d7e642ac8b492d6f1d2d191a"
+DEEPSEEK_API_URL = "https://api.deepseek.com"
--- code/gradio/static.py
+++ code/gradio/static.py
@@ -1,5 +1,3 @@
-DEEPSEEK_API_KEY = "sk-9147b6e1d7e642ac8b492d6f1d2d191a"
-DEEPSEEK_API_URL = "https://api.deepseek.com"
 SYSPROMPT = f"""
     Based on the document line given by the user, generate a QA pair in JSON format.
     Only return valid JSON, nothing else
